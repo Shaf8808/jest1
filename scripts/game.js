3dba97b3e918f7de83e7552d0ff72a1ea78d05b6
Shaf8808 <shadmankhan08@gmail.com>
2023-05-20 13:21:14 +0100

    Add more tests for both game function and newGame

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,10 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"]
 }
 
+function newGame() {
+    game.score= 0
+    game.currentGame = []
+    game.playerMoves = []
+}
 
-
-module.exports = { game }
+module.exports = { game, newGame }
